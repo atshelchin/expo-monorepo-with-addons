ec235e15c5f3c40070816bf1d2ff81d4b7ba2ed3
Shelchin <hi@shelchin.com>
2023-12-23 11:09:41 +0800

    Fix

--- packages/core/src/features/HomeScreen.tsx
+++ packages/core/src/features/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Paragraph, View, Image } from '@acme/ui';
 import { ComponentProps } from 'react';
 import { TextLink } from 'solito/link';
@@ -7,7 +8,7 @@ const blurhash =
 
 export const HomeScreen = () => (
   <View tw="bg-red-200">
-    <Image
+    {/* <Image
       tw="h-12 w-12 flex-none rounded-full bg-green-50"
       source={{
         uri: 'https://psc2.cf2.poecdn.net/8b8a7e1ecc52c932a7ea1e85c5e599b5667d5dc8/_next/static/media/openAIBlue.915c0399.png',
@@ -15,26 +16,33 @@ export const HomeScreen = () => (
       alt=""
     />
 
+*/}
     <Image
       tw="h-12 w-12 flex-none rounded-full bg-green-50"
-      source="https://picsum.photos/seed/696/3000/2000"
+      // source="https://picsum.photos/seed/696/3000/2000"
+
+      source={{
+        width: 250,
+        height: 250,
+        webMaxViewportWidth: 100,
+        uri: 'https://picsum.photos/seed/696/3000/2000',
+      }}
       placeholder={blurhash}
       contentFit="cover"
       transition={1000}
     />
-
     <Image
       tw="h-12 w-12 flex-none rou2nded-full bg-green-50"
-      contentFit="cover"
+      contentFit="contain"
       source={{
         width: 150,
         height: 150,
+        webMaxViewportWidth: '100',
         uri: 'https://cdn.jsdelivr.net/gh/shelchin2023/binance-icons/crypto/sand.svg',
       }}
-      alt=""
     />
 
-    <Paragraph>Hello Expo</Paragraph>
+    <Paragraph>Hello Expo </Paragraph>
     <TextLink href="/404"> Go to 404</TextLink>
   </View>
 );
